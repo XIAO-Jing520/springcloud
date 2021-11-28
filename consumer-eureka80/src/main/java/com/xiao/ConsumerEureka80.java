cf0b72fef93d213bb715ca498c72f6fd10cce145
XIAO-Jing520 <1003620061@qq.com>
2021-11-28 18:59:52 +0800

    学习 rebon 替换默认轮询

--- consumer-eureka80/src/main/java/com/xiao/ConsumerEureka80.java
+++ consumer-eureka80/src/main/java/com/xiao/ConsumerEureka80.java
@@ -1,15 +1,18 @@
 package com.xiao;
 
 
+import com.rule.Rule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "PROVIDER-ORDER-EUREKA",configuration = Rule.class)
 public class ConsumerEureka80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerEureka80.class);
@@ -17,7 +20,7 @@ public class ConsumerEureka80 {
 
     @Bean
     @LoadBalanced      //注解赋予RestTemplate负载均衡的能力
-    RestTemplate restTemplate(){
+    public RestTemplate restTemplate(){
         return new RestTemplate();
     }
 }
