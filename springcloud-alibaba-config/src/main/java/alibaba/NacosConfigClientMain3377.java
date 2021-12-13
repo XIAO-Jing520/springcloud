91b3fd187fc36c0447f5c0bba0d71945f69ab8ef
XIAO-Jing520 <1003620061@qq.com>
2021-12-14 03:15:12 +0800

    学习 nacos-config

---
+++ springcloud-alibaba-config/src/main/java/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Milo
+ * @Description
+ * @Date 2020-05-14 19:18
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
